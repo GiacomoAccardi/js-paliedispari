a74f9435a0772720814beb25bd6f83b5cf588725
giacomohrd <giacomo.accardi.lavoro@gmail.com>
2024-07-04 17:07:02 +0200

    [pari e dispari UPD] There are some instruction into js file

---
+++ pari e dispari/script.js
@@ -0,0 +1,8 @@
+/* 
+1. chiedo all'utente se vuole inserire un numero pari o dispari 
+2. chiedo all'utente di inserire un numero tra 1 e 5
+3. genero un numero causale per conto del computer utilizzando una funzione.
+4. sommo il numero dell'utente + numero randomico 
+5. stabilisco se il risultato è pari o dispari e decreto il vincitore
+*/
+
