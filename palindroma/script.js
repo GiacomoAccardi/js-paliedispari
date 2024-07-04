7d985b0cb45c583453c6d5e449357ef1d013287a
giacomohrd <giacomo.accardi.lavoro@gmail.com>
2024-07-04 16:29:55 +0200

    [Palindroma UPD] There's now some instruction into js file

---
+++ palindroma/script.js
@@ -0,0 +1,8 @@
+/* 
+1. creare una funzione che stabilisca se una parola è palindroma o meno
+2. creare un prompt che permetta l'inserimento di una parola
+3. separare la parola in singoli caratteri e invertirla
+4. utilizzare la funzione per stabilire il risultato
+*/
+
+
